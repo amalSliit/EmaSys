49055e10c1755d296c68e82ac8affc8dc6c71dcc
Amal Ranasinghe <amal@soho.com.au>
2024-10-16 00:42:12 +0530

    called jwttoken sec for Attendance apis

--- src/main/java/com/esad/emasys/controller/AttendanceController.java
+++ src/main/java/com/esad/emasys/controller/AttendanceController.java
@@ -1,12 +1,13 @@
 package com.esad.emasys.controller;
 
-import com.esad.emasys.model.Attendance;
 import com.esad.emasys.model.AttendanceResponse;
+import com.esad.emasys.security.JwtUtil;
 import com.esad.emasys.services.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,15 +20,57 @@ public class AttendanceController {
     @Autowired
     private AttendanceService attendanceService;
 
-    @PostMapping("/checkin/{empId}")
-    public ResponseEntity<AttendanceResponse> checkIn(@PathVariable Integer empId) {
-        LocalDateTime checkInTime = attendanceService.checkIn(empId);
-        return ResponseEntity.ok(new AttendanceResponse("Checked in successfully", checkInTime));
+    private final JwtUtil jwtUtil;
+
+    public AttendanceController(AttendanceService attenService) {
+        this.attendanceService = attenService;
+        this.jwtUtil = new JwtUtil();
     }
 
-    @PostMapping("/checkout/{empId}")
-    public ResponseEntity<AttendanceResponse> checkOut(@PathVariable Integer empId) {
-        LocalDateTime checkOutTime = attendanceService.checkOut(empId);
-        return ResponseEntity.ok(new AttendanceResponse("Checked out successfully", checkOutTime));
+    // Remove "Bearer " prefix if it exists in token
+    private String getFilterToken(String token) {
+        if (token.startsWith("Bearer ")) {
+            return token.substring(7);
+        } else {
+            return token;
+        }
+    }
+
+    @PostMapping("/checkin")
+    public ResponseEntity<AttendanceResponse> checkIn(@RequestHeader("Authorization") String authToken) {
+
+        String token = getFilterToken(authToken);
+
+        if (jwtUtil.validateToken(token)) {
+            // Get employee ID from token
+            int empId = jwtUtil.getEmployeeId(token);
+
+            //save checkIn
+            LocalDateTime checkInTime = attendanceService.checkIn(empId);
+            /*return ResponseEntity.status(HttpStatus.OK)
+                    .body(new AttendanceResponse("Checked in successfully", checkInTime));*/
+            return ResponseEntity.ok(new AttendanceResponse("Checked in successfully", checkInTime));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new AttendanceResponse("Unauthorized Access. Please re-login.", null));
+        }
+    }
+
+    @PostMapping("/checkout")
+    public ResponseEntity<AttendanceResponse> checkOut(@RequestHeader("Authorization") String authToken) {
+
+        String token = getFilterToken(authToken);
+
+        if (jwtUtil.validateToken(token)) {
+            // Get employee ID from token
+            int empId = jwtUtil.getEmployeeId(token);
+
+            //save checkOut
+            LocalDateTime checkOutTime = attendanceService.checkOut(empId);
+            return ResponseEntity.ok(new AttendanceResponse("Checked out successfully", checkOutTime));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new AttendanceResponse("Unauthorized Access. Please re-login.", null));
+        }
     }
 }

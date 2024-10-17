fc37dbad09563bf72302a91beb3e38f627746c9c
Amal Ranasinghe <amal@soho.com.au>
2024-10-18 01:03:38 +0530

    attendance : request model updated

--- src/main/java/com/esad/emasys/controller/AttendanceController.java
+++ src/main/java/com/esad/emasys/controller/AttendanceController.java
@@ -1,11 +1,13 @@
 package com.esad.emasys.controller;
 
+import com.esad.emasys.model.AttendanceRequest;
 import com.esad.emasys.model.AttendanceResponse;
+import com.esad.emasys.model.LoginRequest;
 import com.esad.emasys.services.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,72 +17,24 @@ import java.time.LocalDateTime;
 @RequestMapping("/api/attendance")
 public class AttendanceController {
 
+    private final AttendanceService attendanceService;
+
     @Autowired
-    private AttendanceService attendanceService;
-
-    //private final JwtUtil jwtUtil;
-
-    public AttendanceController(AttendanceService attenService) {
-        this.attendanceService = attenService;
-        //this.jwtUtil = new JwtUtil();
-    }
-
-    // Remove "Bearer " prefix if it exists in token
-    private String getFilterToken(String token) {
-        if (token.startsWith("Bearer ")) {
-            return token.substring(7);
-        } else {
-            return token;
-        }
+    public AttendanceController(AttendanceService attendanceService) {
+        this.attendanceService = attendanceService;
     }
 
     @PostMapping("/checkin")
-    public ResponseEntity<AttendanceResponse> checkIn(@RequestHeader("Authorization") String authToken) {
-
-        String token = getFilterToken(authToken);
-
-        /*if (jwtUtil.validateToken(token)) {
-            // Get employee ID from token
-            int empId = jwtUtil.getEmployeeId(token);
-
-            //save checkIn
-            LocalDateTime checkInTime = attendanceService.checkIn(empId);
-            return ResponseEntity.ok(new AttendanceResponse("Checked in successfully", checkInTime));
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new AttendanceResponse("Unauthorized Access. Please re-login.", null));
-        }*/
-
-        // Get employee ID from token
-        int empId = 40;
-
-        //save checkIn
-        LocalDateTime checkInTime = attendanceService.checkIn(empId);
+    public ResponseEntity<AttendanceResponse> checkIn(@RequestBody AttendanceRequest attendanceRequest) {
+        // Save check-in time
+        LocalDateTime checkInTime = attendanceService.checkIn(attendanceRequest.getEmpId());
         return ResponseEntity.ok(new AttendanceResponse("Checked in successfully", checkInTime));
     }
 
     @PostMapping("/checkout")
-    public ResponseEntity<AttendanceResponse> checkOut(@RequestHeader("Authorization") String authToken) {
-
-        String token = getFilterToken(authToken);
-
-        /*if (jwtUtil.validateToken(token)) {
-            // Get employee ID from token
-            int empId = jwtUtil.getEmployeeId(token);
-
-            //save checkOut
-            LocalDateTime checkOutTime = attendanceService.checkOut(empId);
-            return ResponseEntity.ok(new AttendanceResponse("Checked out successfully", checkOutTime));
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new AttendanceResponse("Unauthorized Access. Please re-login.", null));
-        }*/
-
-        // Get employee ID from token
-        int empId = 40;
-
-        //save checkOut
-        LocalDateTime checkOutTime = attendanceService.checkOut(empId);
+    public ResponseEntity<AttendanceResponse> checkOut(@RequestBody AttendanceRequest attendanceRequest) {
+        // Save check-out time
+        LocalDateTime checkOutTime = attendanceService.checkOut(attendanceRequest.getEmpId());
         return ResponseEntity.ok(new AttendanceResponse("Checked out successfully", checkOutTime));
     }
 }
